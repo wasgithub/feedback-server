b4ad489cb74d57ed06b6118d4e152026115fb0fb
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2022-06-07 22:54:20 -0300

    feature: create project node

---
+++ src/routes.ts
@@ -0,0 +1,29 @@
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import express from "express";
+import nodemailer from "nodemailer";
+
+import { prisma } from "./prisma";
+import { NodemailMailAdapter } from "./adapters/nodemailer/nodemail-mail-adapter";
+
+export const routes = express.Router();
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const nodemailerMailAdapter = new NodemailMailAdapter();
+
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
+
+  return res.status(201).send();
+});
---
+++ src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,34 @@
+import { MailAdapter } from "../adapters/mail-adapter";
+import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+
+interface SubmitFeedbackuseCaseRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+  constructor(
+    private feedbacksRepository: FeedbacksRepository,
+    private mailAdapter: MailAdapter
+  ) {}
+
+  async execute(request: SubmitFeedbackuseCaseRequest) {
+    const { type, comment, screenshot } = request;
+
+    await this.feedbacksRepository.create({
+      type,
+      comment,
+      screenshot,
+    });
+
+    await this.mailAdapter.sendMail({
+      subject: "Novo feedback",
+      body: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+      ].join("\n"),
+    });
+  }
+}
